987e26b278d1017d26e825e9b4a10a29aaf38c6a
Gonçalo Correia <gfcorreia@protonmail.com>
2023-01-17 23:58:03 +0000

    Update neovim

--- neovim/.config/nvim/plugin/plugins.lua
+++ neovim/.config/nvim/plugin/plugins.lua
@@ -65,4 +65,5 @@ return require('packer').startup(function(use)
 	use 'tpope/vim-fugitive'
 	use {"akinsho/toggleterm.nvim", tag = '*', config = function() require("toggleterm").setup() end}
 	use { 'nvim-telescope/telescope-dap.nvim' }
+	use {'vim-airline/vim-airline'}
 end)
