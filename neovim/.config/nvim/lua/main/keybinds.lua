79be76ab97390415f2f858374a2a1db682347e1e
Gonçalo Correia <gcorreia@s21sec.com>
2023-01-19 18:01:43 +0000

    Update neovim: telescope, lsp, small config changes

--- neovim/.config/nvim/lua/main/keybinds.lua
+++ neovim/.config/nvim/lua/main/keybinds.lua
@@ -11,9 +11,13 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
+-- TODO: search what is this `zz`
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "<C-j>", "<C-d>", { noremap = true } )
+vim.keymap.set("n", "<C-k>", "<C-u>zz", { noremap = true } )
 vim.keymap.set("n", "N", "Nzzzv")
 
 vim.keymap.set("n", "<leader>vwm", function()
